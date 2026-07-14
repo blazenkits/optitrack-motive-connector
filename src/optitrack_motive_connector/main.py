dfbb3881450b54886b48ef56bcfc22649ac46abe
Sungmin Choe <77095432+blazenkits@users.noreply.github.com>
2026-07-14 18:26:24 +0900

    commit

--- src/optitrack_motive_connector/main.py
+++ src/optitrack_motive_connector/main.py
@@ -5,10 +5,11 @@ if __name__ == "__main__":
     # e.g. Start Recording -> 3초간 Log -> Stop Recording -> Flush
     with context.safeguard:
         context.init()
-        context.set_capture_config(...)
         context.connect()
         context.start_recording()
         context.capture(3)
         context.sleep(2)
         context.stop_recording()
-        context.to_dataframe()      # Should have 3 second worth of logs
+        df = context.to_dataframe()      # Should have 3 second worth of logs
+        print(df.head)
+        print(df.tail)

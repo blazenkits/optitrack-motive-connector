d3a1ccb73c92bb2888211b027b09224ada182ab7
Sungmin Choe <77095432+blazenkits@users.noreply.github.com>
2026-07-14 15:15:15 +0900

    Initial commit

---
+++ src/optitrack_motive_connector/main.py
@@ -0,0 +1,10 @@
+from natnet import NatNetClient
+
+client = NatNetClient(
+    server_ip_address="127.0.0.1",
+    local_ip_address="127.0.0.1",
+    use_multicast=False
+)
+
+with client:
+    print("Connected!")
